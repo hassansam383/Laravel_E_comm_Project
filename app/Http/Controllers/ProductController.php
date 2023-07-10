6cbe82e029838e4ba567c84700b29ce5ae055a24
“devops_enginer” <mohammad.h@start-tech.ae>
2023-07-10 13:15:54 +0500

    Completed the remove from cart functionality

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -51,4 +51,9 @@ class ProductController extends Controller
 
         return view('cartlist',['products'=>$products]);
     }
+    function removeCart($id)
+    {
+        Cart::destroy($id);
+        return redirect('cartlist');
+    }
 }
